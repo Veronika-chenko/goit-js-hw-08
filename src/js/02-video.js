e6cf4207276205f1122e78b94ee57b28fdfaa92e
Veronika Kravchenko <aniregma@gmail.com>
2022-08-22 17:39:34 +0300

    js-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(setTime, 1000));
+
+function setTime(data) {
+    localStorage.setItem(CURRENT_TIME, data.seconds)
+}
+
+const currentTime = localStorage.getItem(CURRENT_TIME);
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+}
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+}
+
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+refs.form.addEventListener('input', throttle(getFormData, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+populateTextarea()
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    console.log('submit form');
+    evt.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function getFormData(evt) {
+    formData[evt.target.name] = evt.target.value;
+    console.log(formData);
+
+    const formDataJson = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_KEY, formDataJson);
+} 
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    if (savedMessage) {
+        const parsMessage = JSON.parse(savedMessage);
+        refs.textarea.value = parsMessage.message;
+        refs.input.value = parsMessage.email;
+    }
+}
 
