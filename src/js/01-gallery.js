9e8006006a69547e9da4dd9cb1db7bb9b9f70572
Veronika Kravchenko <aniregma@gmail.com>
2022-08-22 17:30:18 +0300

    deploy test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+import SimpleLightbox from "simplelightbox"; 
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+galleryEl.addEventListener('click', setModal);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" alt="${description}" title="${description}"/>
+        </a>`;
+    })
+    .join('');
+}
+
+function setModal(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+    
+  let gallery = new SimpleLightbox(".gallery a");
+
+  gallery.on("show.simplelightbox", function () {
+    gallery.defaultOptions.captionDelay = 250;
+  });
+}
+
